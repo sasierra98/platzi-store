8f7c6cc7febd30d6f6c3d8a7412a4803a6b04563
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2023-07-27 22:00:55 -0500

    Order Page added

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { XMarkIcon } from "@heroicons/react/24/solid/index.js";
 import './style.css';
 import {useContext} from "react";
+import {Link} from "react-router-dom";
 import {ShoppingCartContext} from "../../Context/index.jsx";
 import {OrderCard} from "../OrderCard/index.jsx";
 import {totalPrice} from "../../utils/index.js";
@@ -55,7 +56,9 @@ export const CheckoutSideMenu = () => {
           <span className={'font-light'}>Total:</span>
           <span className={'font-medium text-2xl'}>${totalPrice(context.cartProducts)}</span>
         </p>
-        <button className={'bg-black py-3 text-white w-full rounded-lg'} onClick={() => handleCheckout()}>Checkout</button>
+        <Link to={'/my-orders/last'}>
+          <button className={'bg-black py-3 text-white w-full rounded-lg'} onClick={() => handleCheckout()}>Checkout</button>
+        </Link>
       </div>
     </aside>
   )
--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,9 +1,27 @@
 import {Layout} from "../../Components/Layout/index.jsx";
+import {OrderCard} from "../../Components/OrderCard/index.jsx";
+import {useContext} from "react";
+import {ShoppingCartContext} from "../../Context/index.jsx";
 
 function MyOrder() {
+  const context = useContext(ShoppingCartContext);
+
   return (
     <Layout>
       My Order
+      <div className={'flex flex-col w-80'}>
+        {
+          context.order?.slice(-1)[0].products.map((product) => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              imageUrl={product.image}
+              price={product.price}
+            />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
